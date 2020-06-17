f4795d9320f07d4d4e3dab4ffd755d3bb95ae138
malikaleem432 <m.aleem9090@gmail.com>
2020-06-17 23:26:53 +0500

    Updates

---
+++ precache-manifest.9c0380a1aaf091d245cfb4080b285775.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e031c5e7c778600fd4751e88cef4d5",
+    "url": "/Aalvi-Alunium/index.html"
+  },
+  {
+    "revision": "daff2046ffbeebf8fa77",
+    "url": "/Aalvi-Alunium/static/css/main.eec19574.chunk.css"
+  },
+  {
+    "revision": "01f404927ef52909a5eb",
+    "url": "/Aalvi-Alunium/static/js/2.392d5685.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Aalvi-Alunium/static/js/2.392d5685.chunk.js.LICENSE"
+  },
+  {
+    "revision": "daff2046ffbeebf8fa77",
+    "url": "/Aalvi-Alunium/static/js/main.57e22e53.chunk.js"
+  },
+  {
+    "revision": "1ffebf5b659690beed12",
+    "url": "/Aalvi-Alunium/static/js/runtime-main.9f627368.js"
+  },
+  {
+    "revision": "1901e637d0935319a24abd824ebc5ad7",
+    "url": "/Aalvi-Alunium/static/media/PizzaLogo.1901e637.png"
+  },
+  {
+    "revision": "f5edaa47492768a9bf341d4ec71243e5",
+    "url": "/Aalvi-Alunium/static/media/aalvi1.f5edaa47.jpg"
+  },
+  {
+    "revision": "c5434110f6ea20c1554d1433a6dda321",
+    "url": "/Aalvi-Alunium/static/media/aalvi2.c5434110.png"
+  },
+  {
+    "revision": "c659bde42641526852938d189482a345",
+    "url": "/Aalvi-Alunium/static/media/aalvi4.c659bde4.jpg"
+  }
+]);
